bd13f8bdfbad2182e1c09e6d8276ea92c6b42028
SysEngPRG <stanislav.mediatech@gmail.com>
2024-06-10 20:03:55 +0500

    Update service_v2.py

--- service_v2.py
+++ service_v2.py
@@ -113,7 +113,7 @@ class Filler(Connection, Values):
 class GsBuild(Values):
 
     def get_service_sacc():
-        creds_json = "GoogleServiseKEY.json"
+        creds_json = "confs/GoogleServiseKEY.json"
         scopes = ['https://www.googleapis.com/auth/spreadsheets']
         creds_service = ServiceAccountCredentials.from_json_keyfile_name(creds_json, scopes).authorize(httplib2.Http())
         return build('sheets', 'v4', http=creds_service)
